11995f0c8565bbc0ad6de57e8ec74cd14617a20a
LintZ <fabianprasetyo133@gmail.com>
2024-08-28 15:58:22 +0700

    Init

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Article::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $article = Article::create($request->all());
+        return $article;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Article::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $article = Article::findOrFail($id);
+        $article->update($request->all());
+        return $article;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Article::findOrFail($id) ->delete();
+
+        return response()->json([null, 204]);
+    }
+}
